30cd3c9fa20ab4e61403494a7808d680d6ed435d
RedRudeBoy <RedRudeBoy@gmail.com>
2013-08-19 21:26:36 +0200

    config is in .gitignore & is secret

--- app/config.js
+++
@@ -1,5 +0,0 @@
-	clientId: '827915641106.apps.googleusercontent.com',//web application
-//	clientId: '827915641106-pv8v261cj864m79jtbsuv3civ1b81j5u.apps.googleusercontent.com', //Installed application
-//	apiKey: 'WN85kTXZ5Ads7TWiCsUfquJT', //client secret web app
-//	apiKey: 'NDzGalbOgz7VW5VIfkpxNOCI', //client secret installed app
-	apiKey2: 'AIzaSyDRJJspJ8iRBkR_z0hjHL53392XwnyVUmc', //Key for browser apps (with referers)
